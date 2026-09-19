f027969001752645c2844b288547dbcbdde5ae67
Rommon <litvinroman60@gmail.com>
2026-09-19 11:10:43 +0300

    Add files via upload

---
+++ devices/ssc377d_fpv_rommon/general/overlay/usr/share/openipc/customizer.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Perform basic settings on a known IP camera
+#
+#
+# Set custom upgrade url
+#
+fw_setenv upgrade 'https://github.com/OpenIPC/builder/releases/download/latest/ssc377d_fpv.tgz'
+#
+
+exit 0
